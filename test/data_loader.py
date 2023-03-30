1a2ca77a3e8525881553c26d2d84e1fb38df26f1
sajjad-sh33 <79309701+sajjad-sh33@users.noreply.github.com>
2023-03-30 16:13:46 +0330

    Update data_loader.py

--- test/data_loader.py
+++ test/data_loader.py
@@ -8,18 +8,20 @@ import numpy as np
 from torchvision.io import read_image
 
 class ADP_Dataset(torch.utils.data.Dataset):
-    def __init__(self, path="/media/z/New Volume/ADP_Dataset", transform=None):
+    def __init__(self, path="./ADP_V1.0_Release/", transform=None):
         'Initialization'
         df = pd.read_csv(path + "/ADP_EncodedLabels_Release1_Flat.csv")
-
-        self.labels = df[df.columns[1:11]].to_numpy()
+        df=df[[df.columns[0],"E",	"C"	,"H",	"S",	"A",	"M",	"N"]] #level 1
+#         df=df[[df.columns[0],"E.M.S","E.M.C","E.T.S","E.T.C","E.T.X","C.D.I","C.D.R","C.L","C.X","H.E","H.K","H.Y","H.X","S.M","S.C","S.R","A.W","A.M","M","N.P","N.R","N.G.M","N.G.X"]] #level (3)
+        df=df.sample(frac=0.1, replace=True, random_state=0)
+        self.labels = df[df.columns[1:]].to_numpy()
 
         self.paths = np.array([os.path.join(path, "img_res_1um_bicubic", i) for i in df['Patch Names']])
         self.transform = transforms.Compose([
                 transforms.Resize(256),
                 transforms.CenterCrop(224),
                 #transforms.ToTensor(),
-                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                transforms.Normalize(mean=[0, 0, 0], std=[255, 255, 255]),
             ])
 
     def __len__(self):
@@ -36,7 +38,7 @@ class ADP_Dataset(torch.utils.data.Dataset):
         if self.transform:
 
             image = self.transform(image.float())
-            image = image.unsqueeze(0)
+          #  image = image.unsqueeze(0)
 
         return image, label
 
@@ -47,8 +49,10 @@ class Dataset:
         if dataset=="ADP":
             from torch.utils.data import DataLoader
             data = ADP_Dataset()
+            train_size=int(len(data)*0.8)
+            val_size=len(data)-train_size
 
-            self.train_dataset, self.test_dataset = torch.utils.data.random_split(data, [15901,1767])
+            self.train_dataset, self.test_dataset = torch.utils.data.random_split(data, [train_size,val_size])
             self.train_loader= DataLoader(self.train_dataset, batch_size=_batch_size, shuffle=True)
             self.test_loader= DataLoader(self.test_dataset , batch_size=_batch_size, shuffle=True)
         if dataset == 'mnist':
@@ -95,5 +99,5 @@ class Dataset:
             test_dataset = smallNORB('./data/SmallNORB', train=False, download=True,
                                           transform=dataset_transform)
 
-            self.train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=_batch_size, shuffle=True)
+            self.train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=_batch_size, shuffle=False)
             self.test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=_batch_size, shuffle=False)
